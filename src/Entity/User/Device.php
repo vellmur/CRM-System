b245d5bcb3318959f79463e9e04dd11c4b0f7f49
Valentine Murnik <valentinemurnik@gmail.com>
2020-04-20 16:02:30 +0300

    Remove migrations from autowire

--- src/Entity/User/Device.php
+++ src/Entity/User/Device.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
+ * 
  * @ORM\Table(name="user__device")
  * @ORM\Entity(repositoryClass="App\Repository\DeviceRepository")
  */
